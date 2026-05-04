4176f56ccf17db7ada21d9a367ab84e190a9f510
Jorge Correa <jorge.correa@kreoint.mx>
2026-05-05 00:40:26 +0200

    fix: register UsersController in auth module

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import type { SignOptions } from "jsonwebtoken";
-import { AuthController } from "./auth.controller.js";
+import { AuthController, UsersController } from "./auth.controller.js";
 import { AuthService } from "./auth.service.js";
 import { JwtStrategy } from "./jwt.strategy.js";
 
@@ -33,7 +33,7 @@ import { JwtStrategy } from "./jwt.strategy.js";
       inject: [ConfigService],
     }),
   ],
-  controllers: [AuthController],
+  controllers: [AuthController, UsersController],
   providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
